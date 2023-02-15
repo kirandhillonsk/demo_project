d604a28abc0514cffa2b293762b226309668fb16
kirandhillonsk <119833514+kirandhillonsk@users.noreply.github.com>
2023-02-15 23:03:00 +0530

    Add files via upload

---
+++ enduser/constants.py
@@ -0,0 +1,29 @@
+ 
+
+'''
+Pagination 
+'''
+PAGE_COUNT = 10
+
+"""
+Pet Type
+"""
+PET_TYPE = ((1, "None"),(1, "Dog"),(2, "Cat"),(3, "Other"))
+NONE = 0
+DOG = 1
+CAT =2
+OTHER = 3
+
+"""
+Pet Gender
+"""
+PET_GENDER = ((1, "Male"),(2, "Female"))
+MALE = 1
+FEMALE = 2
+
+"""
+SERVICE TYPE
+"""
+SERVICE_TYPE = ((1,"NORMAL SERVICE"),(2,"CUSTOM"))
+NORMAL_SERVICE = 1
+CUSTOM_SERVICE = 2
